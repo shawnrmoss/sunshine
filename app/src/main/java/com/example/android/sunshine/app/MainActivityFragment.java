f6fe2a198e1fce8f22533dfa2024e33fbf32352f
Shawn Moss <shawnrmoss@gmail.com>
2015-06-18 17:38:59 -0600

    Progress

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,6 +18,8 @@ import java.util.List;
  */
 public class MainActivityFragment extends Fragment {
 
+    ArrayAdapter<String> mForecastAdapter;
+
     public MainActivityFragment() {
     }
 
@@ -23,8 +27,8 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         // Create some dummy data for the listView.  Represented as "day, weather, high/low"
-
         String[] forecastArray = {
             "Today - Sunny - 88/63",
             "Tommorrow - Foggy - 70/40",
@@ -37,6 +41,23 @@ public class MainActivityFragment extends Fragment {
 
         List<String> weekForecast = new ArrayList<String>(
             Arrays.asList(forecastArray));
+
+        //Now that we have some dummy forecast data, create an ArrayAdapter.
+        //The ArrayAdapter will take data from a source (like our dummy forecast) and
+        //use it to populate the ListView its attached to.
+        mForecastAdapter = new ArrayAdapter<String>(
+          //The current context (this fragment parent activity
+          getActivity(),
+          //ID of the list item layout
+          R.layout.list_item_forecast_textview,
+          //ID of the textview to populate
+          R.id.list_item_forecast_textview,
+          //Forecast data
+          weekForecast);
+
+        //Get a reference to the ListView, and attach this adapter to it
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
 
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
